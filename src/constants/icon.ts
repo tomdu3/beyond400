039759312d160ed7f9c2f0c053945b72c17bab9c
Tomislav Dukez <tomdu3@ymail.com>
2025-04-10 16:03:34 +0100

    update favicon

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -2,43 +2,64 @@ import type { Favicon } from '@/types/config.ts'
 
 export const defaultFavicons: Favicon[] = [
   {
-    src: '/favicon/favicon-light-32.png',
-    theme: 'light',
-    sizes: '32x32',
-  },
-  {
-    src: '/favicon/favicon-light-128.png',
-    theme: 'light',
-    sizes: '128x128',
-  },
-  {
-    src: '/favicon/favicon-light-180.png',
-    theme: 'light',
-    sizes: '180x180',
-  },
-  {
-    src: '/favicon/favicon-light-192.png',
+    src: '/favicon/android-chrome-192x192.png',
     theme: 'light',
     sizes: '192x192',
   },
   {
-    src: '/favicon/favicon-dark-32.png',
-    theme: 'dark',
+    src: '/favicon/apple-touch-icon.png',
+    theme: 'light',
+    sizes: '180x180', // Add appropriate size here if the file is applicable
+  },
+  {
+    src: '/favicon/favicon-32x32.png',
+    theme: 'light',
     sizes: '32x32',
   },
   {
-    src: '/favicon/favicon-dark-128.png',
-    theme: 'dark',
-    sizes: '128x128',
+    src: '/favicon/android-chrome-512x512.png',
+    theme: 'light',
+    sizes: '512x512', // This was not in your original, but you can add it.
   },
   {
-    src: '/favicon/favicon-dark-180.png',
+    src: '/favicon/favicon-16x16.png',
+    theme: 'light',
+    sizes: '16x16',
+  },
+  {
+    src: '/favicon/favicon.ico',
+    theme: 'light',
+    sizes: 'any', 
+  },
+  
+  {
+    src: '/favicon/android-chrome-192x192.png',
+    theme: 'dark',
+    sizes: '192x192',
+  },
+  {
+    src: '/favicon/apple-touch-icon.png',
     theme: 'dark',
     sizes: '180x180',
   },
   {
-    src: '/favicon/favicon-dark-192.png',
+    src: '/favicon/favicon-32x32.png',
     theme: 'dark',
-    sizes: '192x192',
+    sizes: '32x32',
   },
-]
+  {
+    src: '/favicon/android-chrome-512x512.png',
+    theme: 'dark',
+    sizes: '512x512',
+  },
+  {
+    src: '/favicon/favicon-16x16.png',
+    theme: 'dark', 
+    sizes: '16x16',
+  },
+  {
+    src: '/favicon/favicon.ico',
+    theme: 'dark',
+    sizes: 'any',
+  },
+];
